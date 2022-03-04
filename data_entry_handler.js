9f560a5321f0b2eec36dbb3f7a746ab76cb7fbfc
kieferjackson <87453284+kieferjackson@users.noreply.github.com>
2022-03-04 16:53:05 -0700

    Improved data validation and form generation
    Initial data entry no longer accepts empty strings for 'name', and both 'num' and 'molar_eq' no longer accept non-numbers or 0.  
    Fixed the issue where a user could infinitely create forms.

--- data_entry_handler.js
+++ data_entry_handler.js
@@ -5,10 +5,12 @@ class Substituent {
         this.name = name;
         this.num = num;
         this.molar_eq = molar_eq;
+        this.start = start;
+        this.end = end;
     }
 }
 
-class Monomer {
+class Monomer extends Substituent {
     constructor(mass, wpercent, mpercent, molar_mass, moles) {
         this.mass = mass;
         this.wpercent = wpercent;
@@ -19,21 +21,29 @@ class Monomer {
 }
 
 funcStats = [];
+monomerStats = [];
 
 function getInputValues() {
     var inputs = document.getElementsByClassName("input_field");
 
-    let inputsAcceptable = validate(inputs);
+    let inputsAcceptable = checkDataTypes(inputs);
 
     if (inputsAcceptable === false) {
-        console.log(inputsAcceptable);
+        console.log("inputsAcceptable: " + inputsAcceptable);
     } else {
-        let percent_type = percentTypeChecker();
+        console.log("inputsAcceptable: " + inputsAcceptable);
 
         funcStats = [];
     
         for (var i = 0 ; i < 2 ; i++) {
-            funcStats[i] = new Substituent(percent_type, inputs[i + 2].value, parseInt(inputs[i + 4].value), parseFloat(inputs[i + 6].value));
+            funcStats[i] = {
+                percent_type: percentTypeChecker(),
+                name: inputs[i + 2].value,
+                num: parseInt(inputs[i + 4].value),
+                molar_eq: parseFloat(inputs[i + 6].value),
+                start: 0 + parseInt(inputs[4].value) * i,
+                end: parseInt(inputs[4].value) + parseInt(inputs[5].value) * i
+            }; 
         }
     
         console.log(funcStats[0]);
@@ -44,26 +54,72 @@ function getInputValues() {
 
 }
 
-function validate(inputs) {
-    if (typeof (inputs[2].value && typeof inputs[3].value) === 'string') {
+function getDynamicFormData() {
+
+    var dynFormData = document.getElementsByClassName("dyn_input_field");
+    console.log(dynFormData);
+
+    for (var i = 0 ; i < 2 ; i++) {
+        for (var q = funcStats[i].start ; q < funcStats[i].end ; q++) {
+            monomerStats[q] = {
+                mass: parseFloat(dynFormData[0 + q * 3].value),
+                percent: parseFloat(dynFormData[1 + q * 3].value),
+                molar_mass: parseFloat(dynFormData[2 + q * 3].value)
+            };
+
+            console.log(monomerStats[q]);
+        }
+    }
+
+}
+
+function checkDataTypes(inputs) {
+    // String Checker
+    if ((inputs[2].value || inputs[3].value) === "") {
+        console.log("A valid name must be entered");
+        var nameAcceptable = false;
+    } else if (checkParity(inputs[2].value, inputs[3].value) === true) {
+        console.log("A valid name must be entered (cannot be identical)");
+        var nameAcceptable = false;
+    } else if (typeof (inputs[2].value && typeof inputs[3].value) === 'string') {
         var nameAcceptable = true;
     } else {
         var nameAcceptable = false;
     }
 
-    if (inputs[4].value.match(/\d+/) && inputs[5].value.match(/\d+/) != null) {
+    // Integer Checker
+    if ((inputs[4].value || inputs[5].value) == 0) {
+        console.log("0 is not an acceptable input for num.");
+        var numAcceptable = false;
+    } else if (inputs[4].value.match(/\d+/) && inputs[5].value.match(/\d+/) != null) {
         var numAcceptable = true;
     } else {
         var numAcceptable = false;
+        console.log("ERROR - Invalid data at checkDataTypes function (Integer)\nOne of your input fields may be missing a value.");
     }
 
-    if (inputs[6].value.match(/\d+/) && inputs[7].value.match(/\d+/) != null) {
+    // Float Checker
+    if (inputs[6].value == 0 || inputs[7].value == 0) {
+        console.log("0 is not an acceptable input for eq.");
+        var eqAcceptable = false;
+    } else if (inputs[6].value.match(/\d+/) && inputs[7].value.match(/\d+/) != null) {
         var eqAcceptable = true;
     } else {
         var eqAcceptable = false;
+        console.log("ERROR - Invalid data at checkDataTypes function (Float)");
     }
 
     return nameAcceptable && numAcceptable && eqAcceptable;
+}
+
+function checkParity(var1, var2) {
+    if (var1 === var2) {
+        return true;
+    } else if (var1 != var2) {
+        return false;
+    } else {
+        console.log("There was a problem checking parity between var1: " + var1 + " and var2: " + var2);
+    }
 }
 
 function percentTypeChecker() {
--- form_generator.js
+++ form_generator.js
@@ -1,12 +1,23 @@
 
 function generateForm () {
 
+    // Check if there are existing forms generated
+    if (document.querySelector(".dynamic_form").childElementCount > 0) {
+        // Select any dynamic forms that were previously generated
+        let form1 = document.getElementById(funcStats[0].name + "_entry");
+        let form2 = document.getElementById(funcStats[1].name + "_entry");
+        // Remove previous forms to generate a new one
+        form1.remove();
+        form2.remove();
+    }
+    
     var br = document.createElement("br");
 
     for (var i = 0 ; i < 2 ; i++) {
         console.log(funcStats[i]);
         var form = document.createElement("form");
-        form.setAttribute("name", funcStats[i].name + "_entry");
+        form.setAttribute("name", funcStats[i].name + "_entry");    // Should declare a unique form name
+        form.setAttribute("id", funcStats[i].name + "_entry");      // Unique id is generated from user inputted name
         
         // Generate input fields for each monomer to enter mass and/or percent and molar mass. 
         for (var q = 0 ; q < funcStats[i].num ; q++) {
@@ -14,19 +25,19 @@ function generateForm () {
             var mass = document.createElement("input");
             mass.setAttribute("type", "text");
             mass.setAttribute("name", "mass" + funcStats[i].name + "-" + (q + 1));
-            mass.setAttribute("class", "input_field float");
+            mass.setAttribute("class", "dyn_input_field float");
 
             // Generates form for entering percent
             var percent = document.createElement("input");
             percent.setAttribute("type", "text");
             percent.setAttribute("name", "percent" + funcStats[i].name + "-" + (q + 1));
-            percent.setAttribute("class", "input_field float");
+            percent.setAttribute("class", "dyn_input_field float");
 
             // Generates form for entering molar mass
             var molar_mass = document.createElement("input");
             molar_mass.setAttribute("type", "text");
             molar_mass.setAttribute("name", "molar_mass" + funcStats[i].name + "-" + (q + 1));
-            molar_mass.setAttribute("class", "input_field float");
+            molar_mass.setAttribute("class", "dyn_input_field float");
             molar_mass.required = true; // Molar mass MUST always be entered for all monomers.
 
             // Appending input fields to form; this process will be repeated for each functional group for however many monomers it contains (num)
@@ -36,15 +47,16 @@ function generateForm () {
                 form.appendChild(molar_mass);
                 form.appendChild(br.cloneNode());
 
-            var form_location = document.querySelector(".dynamic_form");
+            var form_attachment = document.querySelector(".dynamic_form");
 
-            form_location.append(form);
+            form_attachment.append(form);
         }
     }
     
-    var s = document.createElement("input");
-    s.setAttribute("type", "submit");
-    s.setAttribute("value", "Submit");
+    var s = document.createElement("button");
+    s.setAttribute("type", "button");
+    s.setAttribute("onclick", "getDynamicFormData()");
+    s.textContent = "Submit";
 
     form.appendChild(s);
 
