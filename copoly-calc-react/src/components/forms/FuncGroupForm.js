06e6322b677b1e44db55f1a3c85292d9b230fa43
kieferjackson <kieferleejackson@gmail.com>
2022-12-14 15:18:04 -0700

    Implemented monomer num buttons

--- copoly-calc-react/src/components/forms/FuncGroupForm.js
+++ copoly-calc-react/src/components/forms/FuncGroupForm.js
@@ -1,7 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function FuncGroupForm()
 {
+    const func_groups =
+    [
+        // Functional Group A
+        {
+            letter: 'A',
+            name_placeholder: 'diester',
+            num_placeholder: 1,
+        },
+        // Functional Group B
+        {
+            letter: 'B',
+            name_placeholder: 'diol',
+            num_placeholder: 2,
+        }
+    ];
+
+    // Starting values for Functional Group Form
+    const FUNC_FORM_FIELDS =
+    {
+        // Functional Group A Form Fields
+        funcA_name: '', funcA_num: '', funcA_molar_eq: '',
+        // Functional Group B Form Fields
+        funcB_name: '', funcB_num: '', funcB_molar_eq: ''
+    };
+
+    const [funcGroupsForm, setFuncGroupsForm] = useState(FUNC_FORM_FIELDS);
+
+    const handleFormChange = (event) => {
+        const { name, value } = event.target;
+
+        // Update form field to new value
+        setFuncGroupsForm({ ...funcGroupsForm, [name]: value });
+    }
+
+    // Functions to increment/decrement monomer number fields
+    const subtractMonomerNum = (func_letter) => {
+        const num_field_name = `func${func_letter}_num`;
+        let current_value = parseInt(funcGroupsForm[num_field_name]);
+        
+        if (current_value > 1 && current_value % 1 === 0) 
+        {
+            // The current value is a sufficiently large whole number, decrement it
+            current_value -= 1;
+            setFuncGroupsForm({ ...funcGroupsForm, [num_field_name]: current_value });
+        } 
+        else if (current_value > 2 && current_value % 1 !== 0) 
+        {
+            // The current value is a sufficiently large decimal number, floor and then decrement it
+            current_value = Math.floor(current_value);
+            current_value -= 1;
+            setFuncGroupsForm({ ...funcGroupsForm, [num_field_name]: current_value });
+        } 
+        else if (current_value > 1 && current_value < 2 && current_value % 1 !== 0) 
+        {
+            // The current value is a decimal number between 1 and 2, set it to 1
+            current_value = 1;
+            setFuncGroupsForm({ ...funcGroupsForm, [num_field_name]: current_value });
+        } 
+        else 
+        {
+            // Set unexpected value to 1
+            setFuncGroupsForm({ ...funcGroupsForm, [num_field_name]: 1 });
+        }
+    }
+
+    const addMonomerNum = (func_letter) => {
+        const num_field_name = `func${func_letter}_num`;
+        let current_value = parseInt(funcGroupsForm[num_field_name]);
+        
+        if (current_value % 1 === 0) 
+        {
+            // The current value is a whole number, increment it
+            current_value += 1;
+            setFuncGroupsForm({ ...funcGroupsForm, [num_field_name]: current_value });
+        } 
+        else if (current_value % 1 > 0) 
+        {
+            // The current value is a decimal number, floor and then increment it
+            current_value = Math.floor(current_value);
+            current_value += 1;
+            setFuncGroupsForm({ ...funcGroupsForm, [num_field_name]: current_value });
+        } 
+        else 
+        {
+            // Set unexpected value to 1
+            setFuncGroupsForm({ ...funcGroupsForm, [num_field_name]: 1 });
+        }
+    }
+
     return(
         <div className="form_container">
             <form id="initial_data_entry">
@@ -16,64 +105,55 @@ export default function FuncGroupForm()
                     </label>
                 </section>
                 <section className="ag_box">
-                    <h1>Functional Group A</h1>
-                    <div className="input_block">
-                        <label>
-                            Substituent Name
-                            <input type="text" name="funcA_name" placeholder="e.g. 'diester'" id="funcA_name" className="input_field string" />
-                        </label>
-                    </div>
-                    <br />
-                    <div className="input_block">
-                        <label>
-                            Number of Comonomers
-                            <input type="text" name="funcA_num" placeholder="e.g. '1'" id="funcA_num" className="input_field int" />
-                        </label>
-                        <button 
-                            type="button" name="minus" 
-                            onClick="add_subtractField('funcA_num', 'subtract')" 
-                            className="square_button plus_minus" tabIndex="-1"
-                        >
-                            <div>-</div>
-                        </button>
-                        <button 
-                            type="button" name="plus" 
-                            onClick="add_subtractField('funcA_num', 'add')" 
-                            className="square_button plus_minus" tabIndex="-1"
-                        >
-                            <div>+</div>
-                        </button>
-                    </div>
-                    
-                    <br />
-                    <h1>Functional Group B</h1>
-                    <div className="input_block">
-                        <label>
-                            Substituent Name
-                            <input type="text" name="funcB_name" placeholder="e.g. 'diol'" id="funcB_name" className="input_field string" />
-                        </label>
-                    </div>
-                    <br />
-                    <div className="input_block">
-                        <label>
-                            Number of Comonomers
-                            <input type="text" name="funcB_num" placeholder="e.g. '2'" id="funcB_num" className="input_field int" />
-                        </label>
-                        <button 
-                            type="button" name="minus" 
-                            onClick="add_subtractField('funcB_num', 'subtract')" 
-                            className="square_button plus_minus" tabIndex="-1"
-                        >
-                            <div>-</div>
-                        </button>
-                        <button 
-                            type="button" name="plus" 
-                            onClick="add_subtractField('funcB_num', 'add')" 
-                            className="square_button plus_minus" tabIndex="-1"
-                        >
-                            <div>+</div>
-                        </button>
-                    </div>
+                    {func_groups.map(({ letter, name_placeholder, num_placeholder }) =>
+                        <div key={`func_group_${letter}`}>
+                            <h1>Functional Group {letter}</h1>
+                            <div className="input_block">
+                                <label>
+                                    Substituent Name
+                                    <input 
+                                        type="text" 
+                                        name={`func${letter}_name`} 
+                                        value={funcGroupsForm[`func${letter}_name`]} 
+                                        onChange={handleFormChange}
+                                        placeholder={`e.g. '${name_placeholder}'`} 
+                                        id={`func${letter}_name`} 
+                                        className="input_field string" 
+                                    />
+                                </label>
+                            </div>
+                            <br />
+                            <div className="input_block">
+                                <label>
+                                    Number of Comonomers
+                                    <input 
+                                        type="text" 
+                                        name={`func${letter}_num`} 
+                                        value={funcGroupsForm[`func${letter}_num`]} 
+                                        onChange={handleFormChange}
+                                        placeholder={`e.g. '${num_placeholder}'`} 
+                                        id={`func${letter}_num`} 
+                                        className="input_field int" 
+                                    />
+                                </label>
+                                <button 
+                                    type="button" name="minus" 
+                                    onClick={() => subtractMonomerNum(letter)} 
+                                    className="square_button plus_minus" tabIndex="-1"
+                                >
+                                    <div>-</div>
+                                </button>
+                                <button 
+                                    type="button" name="plus" 
+                                    onClick={() => addMonomerNum(letter)} 
+                                    className="square_button plus_minus" tabIndex="-1"
+                                >
+                                    <div>+</div>
+                                </button>
+                            </div>
+                            <br />
+                        </div>
+                    )}
                 </section>
                 <br />
                 <section className="optional">
